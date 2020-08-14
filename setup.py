5f022500fa2f17f78f988bedb4615d95c502ca00
mkurdian <37507230+mkurdian@users.noreply.github.com>
2020-08-14 22:07:44 +0100

    Packaging and tdd setup.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkweather',
+    version='0.0.1',
+    python_requires='>=3.8.5,<3.9.0',
+    install_requires=[
+        'requests>=2.24.0,<2.25.0',
+        'click>=7.1.2<7.2.0',
+    ],
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    license='MIT'
+)
